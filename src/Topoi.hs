798cbf7a21a96271fa836bc087a92bdbadd54164
zypeh <zypeh.geek@gmail.com>
2019-11-20 00:20:59 +0800

    Cabal config

--- src/Topoi.hs
+++ src/Topoi.hs
@@ -1,4 +1,12 @@
 module Topoi (main) where
 
+import qualified Text.Megaparsec as MP
+import qualified Text.Megaparsec.Char as MP
+import qualified Text.Megaparsec.Char.Lexer as MPL
+
 main :: IO ()
 main = putStrLn "hello from topoi"
+
+-- Should I use String or Text here ?
+-- type TestParser = MP.Parsec ParseError String
+-- instance CanParse (MP.ParsecT ParseError String Identity)
